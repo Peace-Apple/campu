1b8df2f9ce8fe1665cd3fb3bb382de0940aec3d9
peaceapple <astonreba@gmail.com>
2023-09-19 17:34:55 +0300

    Create nested global contexts to reduce rerenders

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './Home';
 import Speakers from './Speakers';
 import { GlobalProvider } from './GlobalState';
+import { FavoriteClickCountProvider } from './FavoriteClickCountContext';
 
 export const ConfigContext = React.createContext();
 
@@ -12,6 +13,7 @@ const pageToShow = (pageName) => {
 };
 
 const configValue = {
+  showSignMeUp: true,
   showSpeakerSpeakingDays: true,
 };
 
@@ -19,7 +21,9 @@ const App = ({ pageName }) => {
   return (
     <ConfigContext.Provider value={configValue}>
       <GlobalProvider>
-        <div>{pageToShow(pageName)}</div>
+        <FavoriteClickCountProvider>
+          <div>{pageToShow(pageName)}</div>
+        </FavoriteClickCountProvider>
       </GlobalProvider>
     </ConfigContext.Provider>
   );
